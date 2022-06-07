8a8d637747b15a8969d1bc10d2828ffc54559a12
Alan Riaño <alanrhoyos@gmail.com>
2022-06-07 00:18:26 -0500

    Improved products listing styles. Added border radius and improved the mobile view.

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -4,19 +4,21 @@ import "./ProductsList.scss";
 
 const ProductsList = ({ productsList }) => (
   <div className="products-list">
-    {productsList.map((product, index) => (
-      <Fragment key={product.id}>
-        <ProductRow
-          title={product.title}
-          imageSource={product.thumbnail}
-          price={product.original_price ?? product.price}
-          stateName={product.address.state_name}
-          productAttribute={product.attributes[0].values[0].name}
-          hasFreeShipping={product.shipping.free_shipping}
-        />
-        {index !== (productsList.length - 1) && <hr />}{/* Don't show the separator for the last product row */}
-      </Fragment>
-    ))}
+    <div className="products-list__container">
+      {productsList.map((product, index) => (
+        <Fragment key={product.id}>
+          <ProductRow
+            title={product.title}
+            imageSource={product.thumbnail}
+            price={product.original_price ?? product.price}
+            stateName={product.address.state_name}
+            productAttribute={product.attributes[0].values[0].name}
+            hasFreeShipping={product.shipping.free_shipping}
+          />
+          {index !== (productsList.length - 1) && <hr />}{/* Don't show the separator for the last product row */}
+        </Fragment>
+      ))}
+    </div>
   </div>
 );
 
