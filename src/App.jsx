2abbfd523c1b44bda7cea2061a9f91ac4843fee7
Alan Riaño <alanrhoyos@gmail.com>
2022-06-06 21:04:16 -0500

    Implement search functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react';
 import SearchHeader from './components/SearchHeader/SearchHeader';
 import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
 import './App.scss';
 
 function App() {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => console.log(results), [results]); // TO DO: REMOVE THIS LATER
+
+  /**
+  * Search for a results in the database given a query.
+  * @param resultsQuantity Quantity of results stored in state. Default is 4.
+  */
+  const searchForResults = (event, resultsQuantity = 4) => {
+    event.preventDefault();
+    setIsLoading(true);
+    fetch(`https://api.mercadolibre.com/sites/MLA/search?q=:${query}`)
+      .then((data) => data.json())
+      .then((parsedData) => setResults(parsedData.results.slice(0, resultsQuantity)))
+      .catch((error) => console.error(error));
+  }
+
   return (
     <div className="app">
       <header>
-        <SearchHeader />
+        <SearchHeader
+          handleSearch={searchForResults}
+          query={query}
+          setQuery={setQuery}
+        />
       </header>
       <nav>
         <Breadcrumbs sections={["Hello", "Other hello", "Just testing"]} />
--- src/components/SearchHeader/SearchHeader.jsx
+++ src/components/SearchHeader/SearchHeader.jsx
@@ -3,21 +3,32 @@ import logo from '../../assets/logo.png';
 import searchIcon from '../../assets/search-icon.png';
 import './SearchHeader.scss';
 
-const SearchHeader = () => {
+const SearchHeader = ({ handleSearch, query, setQuery }) => {
+  const handleOnChange = (event) => {
+    setQuery(event.target.value);
+  }
+
   return (
     <div className='search-header'>
       <div className='search-header__container'>
         <Link to='/'>
           <img src={logo} alt='logo' className='search-header__container__logo' />
         </Link>
-        <div className="search-header__container__input-container">
-          <input
-            className="search-header__container__input-container__input"
-            placeholder='Nunca dejes de buscar'
-            role="search"
+        <form onSubmit={handleSearch} className="search-header__container__input-container">
+            <input
+              className="search-header__container__input-container__input"
+              placeholder='Nunca dejes de buscar'
+              role="search"
+              onChange={handleOnChange}
+              value={query}
+            />
+          <img 
+            src={searchIcon}
+            alt='search'
+            className='search-header__container__input-container__button'
+            onClick={handleSearch}
           />
-          <img src={searchIcon} alt='search' className='search-header__container__input-container__button' />
-        </div>
+        </form>
       </div >
     </div>
   );
