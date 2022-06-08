70225ae8a5a34f49b3d4705233908135a2ad1847
Alan Riaño <alanrhoyos@gmail.com>
2022-06-07 19:20:21 -0500

    Use the Link component instead of having a handleClick function

--- src/components/ProductRow/ProductRow.jsx
+++ src/components/ProductRow/ProductRow.jsx
@@ -1,37 +1,31 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import shippingImage from "../../assets/shipping.png";
 import "./ProductRow.scss";
 
-const ProductRow = ({ title, imageSource, price, stateName, productAttribute, currency, id, hasFreeShipping }) => {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(`/items/${id}`);
-  };
-
-  return (
-    <div className="product-row">
-      <img src={imageSource} alt="Imagen del producto" className="product-row__thumbnail" onClick={handleClick} />
-      <div className="product-row__info">
-        <div className="product-row__info__top">
-          <div className="product-row__info__top__price-and-shipping">
-            <span className="product-row__info__top__price-and-shipping__price">
-              {price.toLocaleString("es-AR", {
-                style: "currency",
-                currency: currency,
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 0,
-              })}
-            </span>
-            {hasFreeShipping && <img src={shippingImage} alt="Envío gratis" />}
-          </div>
-          <span className="product-row__info__top__location">{stateName}</span>
+const ProductRow = ({ title, imageSource, price, stateName, productAttribute, currency, id, hasFreeShipping }) => (
+  <div className="product-row">
+    <Link to={`/items/${id}`}>
+      <img src={imageSource} alt="Imagen del producto" className="product-row__thumbnail" />
+    </Link>
+    <div className="product-row__info">
+      <div className="product-row__info__top">
+        <div className="product-row__info__top__price-and-shipping">
+          <span className="product-row__info__top__price-and-shipping__price">
+            {price.toLocaleString("es-AR", {
+              style: "currency",
+              currency: currency,
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
+          </span>
+          {hasFreeShipping && <img src={shippingImage} alt="Envío gratis" />}
         </div>
-        <p className="product-row__info__title" onClick={handleClick}>{title}</p>
-        <p>{productAttribute}</p>
+        <span className="product-row__info__top__location">{stateName}</span>
       </div>
+      <Link className="product-row__info__title" to={`/items/${id}`}>{title}</Link>
+      <p>{productAttribute}</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default ProductRow;
